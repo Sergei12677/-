df440ebaf1db411cb3df8bc3a364b1f76d08bff8
Sergei12677 <gorod_26@icloud.com>
2024-10-05 19:09:05 +0500

    Add files via upload

---
+++ mobile_1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name = ('Sergei')
+print( name, type ( name))
+age = 47
+print ( age , type (age))
+age = 47 + 1
+print ( age , type ( age))
+is_student = ('True')
+print('is_student = True')
